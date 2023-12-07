346d24620028a50a17a54cffcc241c61246941f4
pakkkek <Kpojlukkreativnyj@gmail.com>
2023-12-07 18:35:31 +0100

    HW was done with errors.

--- DB Entity/DatabaseManager.cs
+++ DB Entity/DatabaseManager.cs
@@ -1,27 +1,49 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DB_Entity
 {
-    public class Student
+    public class Suppliers
     {
-        public int StudentID { get; set; }
-        public string Fullname { get; set; }
-        public string GroupName { get; set; }
-        public decimal AverageGrade { get; set; }
-        public string MinSubject { get; set; }
-        public string MaxSubject { get; set; }
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactInfo { get; set; }
 
-        public Student(int studentID, string fullname, string groupname, decimal averageGrade, string minSubject, string maxSubject)
+        public Suppliers(int supplierID, string supplierName, string contactInfo)
         {
-            StudentID = studentID;
-            Fullname = fullname;
-            GroupName = groupname;
-            AverageGrade = averageGrade;
-            MinSubject = minSubject;
-            MaxSubject = maxSubject;
+            SupplierID = supplierID;
+            SupplierName = supplierName;
+            ContactInfo = contactInfo;
         }
+
+        public Suppliers() { }
     }
+
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductType { get; set; }
+        public int SupplierID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime SupplyDate { get; set; }
+
+        public Products(int productID, string productName, string productType, int supplierID, int quantity, decimal cost, DateTime supplyDate)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            ProductType = productType;
+            SupplierID = supplierID;
+            Quantity = quantity;
+            Cost = cost;
+            SupplyDate = supplyDate;
+        }
+
+        public Products() { }
+    }
+
 
     public class DatabaseManager
     {
@@ -70,6 +92,200 @@ namespace DB_Entity
             SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
             sqlCommand.ExecuteNonQuery();
             await CloseConnection();
+        }
+
+        public async Task<DataTable> ExecuteQueryAsync(string query)
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                await OpenConnection();
+            }
+
+            DataTable dataTable = new DataTable();
+            using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand))
+            {
+                adapter.Fill(dataTable);
+            }
+
+            await CloseConnection();
+
+            return dataTable;
+        }
+
+        public async Task<List<string>> GetAllTypesAsync()
+        {
+            string query = "SELECT DISTINCT ProductType FROM Products";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            List<string> types = new List<string>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                types.Add(row["ProductType"].ToString());
+            }
+
+            return types;
+        }
+
+        public async Task<List<string>> GetAllSuppliersAsync()
+        {
+            string query = "SELECT DISTINCT SupplierName FROM Suppliers";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            List<string> suppliers = new List<string>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                suppliers.Add(row["SupplierName"].ToString());
+            }
+
+            return suppliers;
+        }
+
+        public async Task<Products> GetProductWithMaxQuantityAsync()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY Quantity DESC";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow row = dataTable.Rows[0];
+                return new Products(
+                    Convert.ToInt32(row["ProductID"]),
+                    row["ProductName"].ToString(),
+                    row["ProductType"].ToString(),
+                    Convert.ToInt32(row["SupplierID"]),
+                    Convert.ToInt32(row["Quantity"]),
+                    Convert.ToDecimal(row["Cost"]),
+                    Convert.ToDateTime(row["SupplyDate"])
+                );
+            }
+
+            return null;
+        }
+
+        public async Task<Products> GetProductWithMinQuantityAsync()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY Quantity ASC";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow row = dataTable.Rows[0];
+                return new Products(
+                    Convert.ToInt32(row["ProductID"]),
+                    row["ProductName"].ToString(),
+                    row["ProductType"].ToString(),
+                    Convert.ToInt32(row["SupplierID"]),
+                    Convert.ToInt32(row["Quantity"]),
+                    Convert.ToDecimal(row["Cost"]),
+                    Convert.ToDateTime(row["SupplyDate"])
+                );
+            }
+
+            return null;
+        }
+
+        public async Task<Products> GetProductWithMinCostAsync()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY Cost ASC";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow row = dataTable.Rows[0];
+                return new Products(
+                    Convert.ToInt32(row["ProductID"]),
+                    row["ProductName"].ToString(),
+                    row["ProductType"].ToString(),
+                    Convert.ToInt32(row["SupplierID"]),
+                    Convert.ToInt32(row["Quantity"]),
+                    Convert.ToDecimal(row["Cost"]),
+                    Convert.ToDateTime(row["SupplyDate"])
+                );
+            }
+
+            return null;
+        }
+
+        public async Task<Products> GetProductWithMaxCostAsync()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY Cost DESC";
+            DataTable dataTable = await ExecuteQueryAsync(query);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow row = dataTable.Rows[0];
+                return new Products(
+                    Convert.ToInt32(row["ProductID"]),
+                    row["ProductName"].ToString(),
+                    row["ProductType"].ToString(),
+                    Convert.ToInt32(row["SupplierID"]),
+                    Convert.ToInt32(row["Quantity"]),
+                    Convert.ToDecimal(row["Cost"]),
+                    Convert.ToDateTime(row["SupplyDate"])
+                );
+            }
+
+            return null;
+        }
+
+        public async Task InsertProductAsync(Products product)
+        {
+            string query = $"INSERT INTO Products (ProductName, ProductType, SupplierID, Quantity, Cost, SupplyDate) " +
+                           $"VALUES ('{product.ProductName}', '{product.ProductType}', {product.SupplierID}, " +
+                           $"{product.Quantity}, {product.Cost}, '{product.SupplyDate.ToString("yyyy-MM-dd HH:mm:ss")}')";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task InsertTypeAsync(string productType)
+        {
+            string query = $"INSERT INTO ProductTypes (ProductType) VALUES ('{productType}')";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task InsertSupplierAsync(Suppliers supplier)
+        {
+            string query = $"INSERT INTO Suppliers (SupplierName, ContactInfo) VALUES ('{supplier.SupplierName}', '{supplier.ContactInfo}')";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task UpdateProductAsync(Products product)
+        {
+            string query = $"UPDATE Products SET ProductName = '{product.ProductName}', ProductType = '{product.ProductType}', " +
+                           $"SupplierID = {product.SupplierID}, Quantity = {product.Quantity}, Cost = {product.Cost}, " +
+                           $"SupplyDate = '{product.SupplyDate.ToString("yyyy-MM-dd HH:mm:ss")}' WHERE ProductID = {product.ProductID}";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task UpdateSupplierAsync(Suppliers supplier)
+        {
+            string query = $"UPDATE Suppliers SET SupplierName = '{supplier.SupplierName}', ContactInfo = '{supplier.ContactInfo}' " +
+                           $"WHERE SupplierID = {supplier.SupplierID}";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task UpdateTypeAsync(string oldType, string newType)
+        {
+            string query = $"UPDATE ProductTypes SET ProductType = '{newType}' WHERE ProductType = '{oldType}'";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            string query = $"DELETE FROM Products WHERE ProductID = {productId}";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task DeleteSupplierAsync(int supplierId)
+        {
+            string query = $"DELETE FROM Suppliers WHERE SupplierID = {supplierId}";
+            await ExecuteNonQuery(query);
+        }
+
+        public async Task DeleteTypeAsync(string productType)
+        {
+            string query = $"DELETE FROM ProductTypes WHERE ProductType = '{productType}'";
+            await ExecuteNonQuery(query);
         }
     }
 }
--- Stock ADONET/MainWindow.xaml.cs
+++ Stock ADONET/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DB_Entity;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,7 +10,7 @@ namespace Stock_ADONET
 {
     public partial class MainWindow : Window
     {
-        const string connectionString = @"Data Source=pakkkek\SQLEXPRESS;Initial Catalog=Stock;Integrated Security=True;Encrypt=False";
+        const string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Stock;Integrated Security=True;Encrypt=False";
         DatabaseManager databaseManager;
 
         public MainWindow()
@@ -18,7 +19,7 @@ namespace Stock_ADONET
             databaseManager = new DatabaseManager(connectionString);
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void ConnectionButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -28,8 +29,10 @@ namespace Stock_ADONET
                 {
                     MessageBox.Show("Connection successful!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
 
-                    List<Student> students = await LoadStudents();
-                    StockUsers.ItemsSource = students;
+                    List<Suppliers> suppliers = await LoadSuppliers();
+                    Suppliers.ItemsSource = suppliers;
+                    List<Products> products = await LoadProducts();
+                    Products.ItemsSource = products;
                 }
             }
             catch (Exception ex)
@@ -42,26 +45,286 @@ namespace Stock_ADONET
             }
         }
 
-        private async Task<List<Student>> LoadStudents()
+        private async void DisconnectionButton_Click(object sender, RoutedEventArgs e)
         {
-            List<Student> students = new List<Student>();
-            string query = "SELECT * FROM Students";
+            await databaseManager.CloseConnection();
+
+            if (databaseManager.sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                MessageBox.Show("Disconnection successful!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                Suppliers.ItemsSource = null;
+                Products.ItemsSource = null;
+            }
+        }
+
+        private async Task<List<Suppliers>> LoadSuppliers()
+        {
+            List<Suppliers> suppliers = new List<Suppliers>();
+            string query = "SELECT * FROM Suppliers";
 
             using (SqlCommand sqlCommand = new SqlCommand(query, databaseManager.sqlConnection))
             using (SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync())
             {
                 while (await sqlDataReader.ReadAsync())
                 {
-                    students.Add(new Student(
+                    suppliers.Add(new Suppliers(
+                        (int)sqlDataReader[0],
+                        sqlDataReader[1].ToString(),
+                        sqlDataReader[2].ToString()));
+                }
+            }
+            return suppliers;
+        }
+
+        private async Task<List<Products>> LoadProducts()
+        {
+            List<Products> products = new List<Products>();
+            string query = "SELECT * FROM Products";
+
+            using (SqlCommand sqlCommand = new SqlCommand(query, databaseManager.sqlConnection))
+            using (SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync())
+            {
+                while (await sqlDataReader.ReadAsync())
+                {
+                    products.Add(new Products(
                         (int)sqlDataReader[0],
                         sqlDataReader[1].ToString(),
                         sqlDataReader[2].ToString(),
-                        (decimal)sqlDataReader[3],
-                        sqlDataReader[4].ToString(),
-                        sqlDataReader[5].ToString()));
+                        (int)sqlDataReader[3],
+                        (int)sqlDataReader[4],
+                        (decimal)sqlDataReader[5],
+                        sqlDataReader.GetDateTime(6)));
                 }
             }
-            return students;
+            return products;
+        }
+
+        private List<Products> ConvertDataTableToProductsList(DataTable dataTable)
+        {
+            List<Products> list = new List<Products>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                list.Add(new Products(
+                    Convert.ToInt32(row["ProductId"]),
+                    row["ProductName"].ToString(),
+                    row["ProductType"].ToString(),
+                    Convert.ToInt32(row["SupplierId"]),
+                    Convert.ToInt32(row["Quantity"]),
+                    Convert.ToDecimal(row["Cost"]),
+                    Convert.ToDateTime(row["SupplyDate"])
+                ));
+            }
+
+            return list;
+        }
+
+        private async void ShowAllInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM Products P INNER JOIN StockMovements SM ON P.ProductId = SM.ProductId";
+            DataTable dataTable = await databaseManager.ExecuteQueryAsync(query);
+            Products.ItemsSource = ConvertDataTableToProductsList(dataTable);
+        }
+
+        private async void ShowAllTypesButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> types = await databaseManager.GetAllTypesAsync();
+            ShowResultInMessageBox("All Types", types);
+        }
+
+        private async void ShowAllSuppliersButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> suppliers = await databaseManager.GetAllSuppliersAsync();
+            ShowResultInMessageBox("All Suppliers", suppliers);
+        }
+
+        private async void ShowMaxQuantityButton_Click(object sender, RoutedEventArgs e)
+        {
+            Products product = await databaseManager.GetProductWithMaxQuantityAsync();
+            ShowResultInMessageBox("Product with Max Quantity", product);
+        }
+
+        private async void ShowMinQuantityButton_Click(object sender, RoutedEventArgs e)
+        {
+            Products product = await databaseManager.GetProductWithMinQuantityAsync();
+            ShowResultInMessageBox("Product with Min Quantity", product);
+        }
+
+        private async void ShowMinCostButton_Click(object sender, RoutedEventArgs e)
+        {
+            Products product = await databaseManager.GetProductWithMinCostAsync();
+            ShowResultInMessageBox("Product with Min Cost", product);
+        }
+
+        private async void ShowMaxCostButton_Click(object sender, RoutedEventArgs e)
+        {
+            Products product = await databaseManager.GetProductWithMaxCostAsync();
+            ShowResultInMessageBox("Product with Max Cost", product);
+        }
+
+        private void ShowResultInMessageBox(string title, object result)
+        {
+            MessageBox.Show($"{title}: {result}", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private async void InsertProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Products newProduct = new Products
+                {
+                    ProductName = ProductNameTextBox.Text,
+                    ProductType = ProductTypeTextBox.Text,
+                    SupplierID = Convert.ToInt32(SupplierIDProductTextBox.Text),
+                    Quantity = Convert.ToInt32(QuantityTextBox.Text),
+                    Cost = Convert.ToDecimal(CostTextBox.Text),
+                    SupplyDate = Convert.ToDateTime(SupplyDateTextBox.Text)
+                };
+
+                await databaseManager.InsertProductAsync(newProduct);
+                MessageBox.Show("Product inserted successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Refresh the DataGrid
+                List<Products> products = await LoadProducts();
+                Products.ItemsSource = products;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error inserting product: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void InsertSupplierButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Suppliers newSupplier = new Suppliers
+                {
+                    SupplierName = SupplierNameTextBox.Text,
+                    ContactInfo = ContactInfoTextBox.Text
+                };
+
+                await databaseManager.InsertSupplierAsync(newSupplier);
+                MessageBox.Show("Supplier inserted successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Refresh the DataGrid
+                List<Suppliers> suppliers = await LoadSuppliers();
+                Suppliers.ItemsSource = suppliers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error inserting supplier: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void UpdateProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Products selectedProduct = (Products)Products.SelectedItem;
+
+                if (selectedProduct != null)
+                {
+                    selectedProduct.ProductName = ProductNameTextBox.Text;
+                    selectedProduct.ProductType = ProductTypeTextBox.Text;
+                    selectedProduct.SupplierID = Convert.ToInt32(SupplierIDTextBox.Text);
+                    selectedProduct.Quantity = Convert.ToInt32(QuantityTextBox.Text);
+                    selectedProduct.Cost = Convert.ToDecimal(CostTextBox.Text);
+                    selectedProduct.SupplyDate = Convert.ToDateTime(SupplyDateTextBox.Text);
+
+                    await databaseManager.UpdateProductAsync(selectedProduct);
+                    MessageBox.Show("Product updated successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    List<Products> products = await LoadProducts();
+                    Products.ItemsSource = products;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a product to update.", "System Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating product: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void UpdateSupplierButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Suppliers selectedSupplier = (Suppliers)Suppliers.SelectedItem;
+
+                if (selectedSupplier != null)
+                {
+                    selectedSupplier.SupplierName = SupplierNameTextBox.Text;
+                    selectedSupplier.ContactInfo = ContactInfoTextBox.Text;
+
+                    await databaseManager.UpdateSupplierAsync(selectedSupplier);
+                    MessageBox.Show("Supplier updated successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    List<Suppliers> suppliers = await LoadSuppliers();
+                    Suppliers.ItemsSource = suppliers;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a supplier to update.", "System Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating supplier: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void DeleteProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Products selectedProduct = (Products)Products.SelectedItem;
+
+                if (selectedProduct != null)
+                {
+                    await databaseManager.DeleteProductAsync(selectedProduct.ProductID);
+                    MessageBox.Show("Product deleted successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    List<Products> products = await LoadProducts();
+                    Products.ItemsSource = products;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a product to delete.", "System Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting product: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void DeleteSupplierButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Suppliers selectedSupplier = (Suppliers)Suppliers.SelectedItem;
+
+                if (selectedSupplier != null)
+                {
+                    await databaseManager.DeleteSupplierAsync(selectedSupplier.SupplierID);
+                    MessageBox.Show("Supplier deleted successfully!", "System Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    List<Suppliers> suppliers = await LoadSuppliers();
+                    Suppliers.ItemsSource = suppliers;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a supplier to delete.", "System Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting supplier: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
